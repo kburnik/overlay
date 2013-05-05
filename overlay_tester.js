67fbefd83d830d0085dcffa9d413c11c8fc080c9
Kristijan Burnik <kristijanburnik@gmail.com>
2013-05-06 01:38:34 +0200

    Start work on overlay

---
+++ overlay_tester.js
@@ -0,0 +1,12 @@
+$(function() {
+	$('#overlay1').overlay({
+		triggerElement:'#button1'
+	});
+	
+	$('#overlay2').overlay({
+		triggerElement:'#button2'
+	});
+	
+	
+//	$('.overlay').overlay("show");
+});
